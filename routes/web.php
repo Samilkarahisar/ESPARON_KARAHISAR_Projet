cd040a201acc9800d318f079a0721888e761b59b
KyleESP <espkyle@gmail.com>
2019-05-14 14:43:06 +0200

    Parties model + avancées 2

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,32 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => ['order']], function () {
+    Route::get('/', function () {
+        return view('welcome');
+    })->name('welcome');
+
+    Route::get('/products/{productCategory}', 'ProductController@index')->name('product');
+
+    Route::get('/product/{product}', 'ProductController@show')->name('product.show');
+
+    Auth::routes();
+
+    Route::group(['middleware' => ['admin']], function () {
+        Route::prefix('/admin')->group(function () {
+            Route::get('/', 'AdminController@index')->name('admin');
+        });
+    });
 });
+
+Route::prefix('/shopping_cart')->group(function () {
+    Route::get('/', 'ShoppingCartController@index')->name('shopping_cart')->middleware('order');
+
+    //Ajax
+    Route::post('/add', 'ShoppingCartController@add')->name('shopping_cart.add');
+
+    Route::post('/update', 'ShoppingCartController@update')->name('shopping_cart.update');
+
+    Route::post('/delete', 'ShoppingCartController@delete')->name('shopping_cart.delete');
+});
+
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .js('resources/js/product.js', 'public/js')
+    .js('resources/js/bag.js', 'public/js')
+    .sass('resources/sass/shopping_cart.scss', 'public/css')
    .sass('resources/sass/app.scss', 'public/css');
